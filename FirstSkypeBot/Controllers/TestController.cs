993b76f92abd7d1a25ccdfd00003cc9ff96346df
roman.koliada <koljadar@gmail.com>
2017-02-23 23:35:21 +0200

    Add TestController

---
+++ FirstSkypeBot/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace FirstSkypeBot.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("test")]
+        public string Test()
+        {
+            return "Hello!";
+        }
+    }
+}
